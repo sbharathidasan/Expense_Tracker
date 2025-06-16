7d71d51146acf960f8eee3b6768e3fce16e4de0f
sbharathidasan <bharathidasan2072005@gmail.com>
2025-06-16 19:31:50 +0530

    created the basic storage in list of dict trouble in converting it to json file

--- demo/expense tracker.py
+++ demo/expense tracker.py
@@ -1,27 +1,49 @@
 import datetime
-
+import json
 name=input("ENTER YOUR NAME")
 
 Bal=int(input("ENTER YOUR BALANCE"))
 data=[]
 id=0
-def BalanceAdd_enquiry():    
-    val=int(input("Enter the Income to add"))
+def BalanceAdd_enquiry(Bal,val):    
     data.append({"id":id,"name":name,"transaction":{
-        "income":Bal+val,
-        "date":datetime.datetime
+        "income":val,
+        "balance":Bal,
+        "date":datetime.datetime,
+        "note":note
     }})
+    return data
+
+def Expense_enquiry(Bal,expense):
+    note=input("enter the note")
+    data.append({"id":id,"name":name,"transaction":{
+            "expense":expense,"balance":Bal,
+            "date":datetime.datetime.now,
+            "note":note
+        }})
+    return data
     
-    print(data[0]["transaction"]["income"])
-    print(data[0]["transaction"]["date"])
+
 while(True):
     choice=int(input("Enter 1 for add Income Enter 2 for Expence Enter 3 for exit "))
     if(choice==1):
-        BalanceAdd_enquiry()
+        val=int(input("Enter the Income to add"))
+        note=input("Enter purpose")
+        Bal+=val
+        data=BalanceAdd_enquiry(Bal,val)
     elif(choice==2):
-        Expence_enquiry()
+        print(Bal)
+        expense=int(input("Enter the Expense to add"))
+        if(expense<Bal):
+            Bal-=expense
+            data=Expense_enquiry(Bal,expense)
+        else:
+            print("--:Expense Exceeds  your Balance:-- ")
+    
     else:
         break
+    json_obj=json.dumps(data)
+    print(json_obj)
 
 """dict structure
 {id:some id, name:"given name",transaction:{
