8c82c5e24fe58041e2c8b9e601cc5e50df02b78b
sbharathidasan <bharathidasan2072005@gmail.com>
2025-06-17 19:46:00 +0530

    created and stored the user data in to the json file

--- demo/expense tracker.py
+++ demo/expense tracker.py
@@ -1,11 +1,18 @@
 import datetime
 import json
-name=input("ENTER YOUR NAME")
-
-Bal=int(input("ENTER YOUR BALANCE"))
-data=[]
-id=0
-def BalanceAdd_enquiry(Bal,val):    
+with open("User.json",'r')as file:
+    data=json.load(file)
+if(len(data)!=0):
+    id=data[len(data)-1]['id']
+    name=data[len(data)-1]['name']
+    Bal=data[len(data)-1]['transaction']['balance']
+    print(id)
+    print(name)
+else:
+    name=input("ENTER YOUR NAME")
+    Bal=int(input("ENTER YOUR BALANCE"))
+    id=-1
+def BalanceAdd_enquiry(Bal,val,id):    
     data.append({"id":id,"name":name,"transaction":{
         "income":val,
         "balance":Bal,
@@ -14,7 +21,7 @@ def BalanceAdd_enquiry(Bal,val):
     }})
     return data
 
-def Expense_enquiry(Bal,expense):
+def Expense_enquiry(Bal,expense,id):
     note=input("enter the note")
     data.append({"id":id,"name":name,"transaction":{
             "expense":expense,"balance":Bal,
@@ -27,23 +34,26 @@ def Expense_enquiry(Bal,expense):
 while(True):
     choice=int(input("Enter 1 for add Income Enter 2 for Expence Enter 3 for exit "))
     if(choice==1):
+        id+=1
         val=int(input("Enter the Income to add"))
         note=input("Enter purpose")
         Bal+=val
-        data=BalanceAdd_enquiry(Bal,val)
+        data=BalanceAdd_enquiry(Bal,val,id)
     elif(choice==2):
         print(Bal)
         expense=int(input("Enter the Expense to add"))
         if(expense<Bal):
+            id+=1
             Bal-=expense
-            data=Expense_enquiry(Bal,expense)
+            data=Expense_enquiry(Bal,expense,id)
         else:
             print("--:Expense Exceeds  your Balance:-- ")
     
     else:
         break
-    json_obj=json.dumps(data)
-    print(json_obj)
+    print(data)
+with open('User.json','w') as json_file:
+    json.dump(data, json_file, indent=4)
 
 """dict structure
 {id:some id, name:"given name",transaction:{
